7bd04ebbee6ac343f1fb6b1b1fdc8347d92ac31c
ChristoforosKotsios <35273070+ChristoforosKotsios@users.noreply.github.com>
2018-02-22 19:57:35 +0200

    monthlycalendar.py
    monthlycalendar.py

---
+++ monthlycalendar.py
@@ -0,0 +1,10 @@
+#Python 2.7.12
+#Christoforos Kotsios for UniPi
+#Email: c.kotsios@gmail.com
+
+import calendar
+
+yy= int(input("Enter a year: \n"))
+mm= int(input("Enter a month: \n"))
+
+print (calendar.month(yy, mm))
